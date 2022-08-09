6cc5290c71acb3dd662aff0a789bef84773c050d
franco-martucci <franco.martucci@arcadiatech.io>
2022-08-09 19:21:50 -0300

    Add basic support for fetching user profiles and user posts

---
+++ src/identity/parse_response.ts
@@ -0,0 +1,61 @@
+import { Profile, ProfileUpdate, Urn } from '../model/api';
+
+interface RawProfileContainer {
+  profile?: {
+    entityUrn?: Urn;
+  };
+}
+
+export const parseProfile = (data: unknown | null): Profile | null => {
+  const output = {
+    profileUrn: (data as RawProfileContainer)?.profile?.entityUrn,
+  };
+  if (!output.profileUrn) {
+    return null;
+  }
+  return output;
+};
+
+interface RawProfileUpdate {
+  actor: Record<string, unknown>;
+  dashEntityUrn: Urn;
+  resharedUpdate?: Record<string, unknown> & {
+    commentary?: { text?: { text: string } };
+  };
+  updateMetadata: { urn: string };
+  entityUrn: Urn;
+  commentary?: { text?: { text: string } };
+  socialDetail: Record<string, unknown>;
+}
+
+export const parseProfileUpdate = (
+  data: unknown | null,
+): ProfileUpdate | null => {
+  const output = {
+    updateUrn: (data as RawProfileUpdate)?.updateMetadata?.urn,
+    text: (data as RawProfileUpdate)?.commentary?.text?.text,
+    resharedText: (data as RawProfileUpdate)?.resharedUpdate?.commentary?.text
+      ?.text,
+  };
+  if (!output.updateUrn || (!output.text && !output.resharedText)) {
+    return null;
+  }
+  if (!output.text) delete output.text;
+  if (!output.resharedText) delete output.resharedText;
+  return output;
+};
+
+interface RawProfileUpdatesContainer {
+  metadata?: Record<string, unknown>;
+  elements?: RawProfileUpdate[];
+  paging?: Record<string, unknown>;
+}
+
+export const parseProfileUpdates = (
+  data: unknown | null,
+): ProfileUpdate[] | null => {
+  const output = (data as RawProfileUpdatesContainer)?.elements.map((el) =>
+    parseProfileUpdate(el),
+  );
+  return output;
+};
--- src/model/urls.ts
+++ src/model/urls.ts
@@ -3,4 +3,6 @@ export const AUTHENTICATE_URL = `${BASE_URL}/uas/authenticate`;
 export const API_BASE_URL = `${BASE_URL}/voyager/api`;
 export const PROFILE_DETAILS = `${API_BASE_URL}/me`;
 export const CONVERSATION_URL = `${API_BASE_URL}/messaging/conversations`;
+export const PROFILE_URL = `${API_BASE_URL}/identity/profiles`;
+export const PROFILE_UPDATES_URL = `${API_BASE_URL}/identity/profileUpdatesV2`;
 export const REALTIME_URL = `${BASE_URL}/realtime/connect`;
